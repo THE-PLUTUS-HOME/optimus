1a6861fa5c7e2480550d0e6a0ddd91f8f05f545c
Young Plutus <malicksamah3@gmail.com>
2025-01-03 09:43:38 -0500

    update

--- src/main/java/com/theplutushome/optimus/config/RestClientConfig.java
+++ src/main/java/com/theplutushome/optimus/config/RestClientConfig.java
@@ -3,15 +3,8 @@ package com.theplutushome.optimus.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 
 @Configuration
 public class RestClientConfig {
